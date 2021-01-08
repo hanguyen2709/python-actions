7fdefbe7833ac455c1f88acbf5e4989f501ff1ef
haNguyen <chgiahuy2709@gmail.com>
2021-01-08 10:55:52 +0700

    change in six

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import six
 app = Flask(__name__)
+
 
 app.route("/")
 def index():
     return "hello, World"
 
 if __name__ == "__main__":
+    if isinstance(value, six.string_types):
+        handle_string(value)
     app.run()
